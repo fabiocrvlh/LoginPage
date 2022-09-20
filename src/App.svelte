cdfd3dafad6fb4f433c7244441633d9a510935ec
FabioCarvalho <carvalhofabiodonato@gmail.com>
2022-09-19 21:55:51 -0300

    Adiciona nova logo

--- src/App.svelte
+++ src/App.svelte
@@ -21,11 +21,17 @@
   style="background-image: url(https://www.pdvg.it/wp-content/uploads/2019/07/dying-light-2.jpg);"
 >
   <div>
-    <img
-      src="https://w7.pngwing.com/pngs/511/894/png-transparent-dying-light-the-following-playstation-4-video-game-xbox-one-minecraft-game-text-graphic.png"
-      alt="logo"
-      class="flex ml-auto mr-auto w-90 h-70 mb-10"
-    />
+    <svg 
+     xmlns="http://www.w3.org/2000/svg"
+     
+     fill="none" 
+     viewBox="0 0 24 24" 
+     stroke-width="1.5" 
+     stroke="currentColor"
+     class=" flex w-48 h-48 m-auto">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>
+    
   </div>
   <div class="py-18 ">
     <div class=" bg-black bg-opacity-90 py-4  ">
